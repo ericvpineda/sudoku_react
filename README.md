dfec44875b705642d9efe9b13e4544aafecd3eac
Eric Pineda <ericvpineda@berkeley.edu>
2022-10-22 15:36:59 -0700

    implemented solver backtracking algor

--- README.md
+++ README.md
@@ -1,11 +1,37 @@
 Working on: 
-- Set up files and basic UI 
+- complete chooseMode fxn
+- implement mutable vs non-mutable cells
+- implement buttons 
+    - new, mode, solve, 
+    - hint, erase
+- implement buttons
+- implement cell highlight + arrow key input 
+- modal overlay once successfuly complete
+- implement time + persistence 
+- add title + fix styling
+    - board higher than control buttons + num pad 
+    - make control btn + num pad more transparent
+    - make cell numbers larger 
+    - style buttons  
+- add public readme.me with summary   
+
+Completed:
+- set up files and basic UI 
+- complete sudoku solver fxn
 
 Issues:
 - Control buttons shift below when page shinks small enough
 
+Solved:
+
 Future Ideas:
 - Create settings page (left-hand)
+- Have tests for utilites and other fxns
+- navbar? 
+- Leaderboard based on difficulty
+- solver fxn visual
+- add more randomness to board creation
+- make n x n grid 
 
 Nice things to have: 
 - bash file with simplified git commands
--- src/utils/index.js
+++ src/utils/index.js
@@ -23,17 +23,84 @@ const shuffleArray = (array) => {
     return array;
 }
 
+//   Note: Only 9! different combinations of boards
 const randomGrid = () => {
   const grid = emptyGrid();
   const numList = [1,2,3,4,5,6,7,8,9];
   grid[0] = shuffleArray(numList)
-  return solveGrid(grid);
+  solveGrid(grid, 1, 0);
+  return grid;
 };
 
-const solveGrid = (grid) => {
-    return grid;
+const validateGrid = (grid, target, row, col) => {
+    const n = grid.length;
+    // Check if row is valid
+    if (grid[row].includes(target)) {
+        return false;
+    } 
+
+    // Check if column is valid 
+    for (let i = 0; i < n; i++) {
+        if (grid[i][col] === target) {
+            return false;
+        }
+    }
+
+    // Check if 3 x 3 block is valid
+    let blockRow = Math.floor(row / 3) * 3
+    let blockCol = Math.floor(col / 3) * 3
+
+    for (let i = blockRow; i < blockRow + 3; i++) {
+        for (let j = blockCol; j < blockCol + 3; j++) {
+
+            if (grid[i][j] === target) {
+                return false;
+            }
+    }}
+
+    return true; 
+}
+
+// Note: Input grid should be original grid (non-modified by user)
+const solveGrid = (grid, row, col) => {
+    
+    let n = grid.length;
+
+    if (col === n) {
+        row += 1 
+        col = 0
+    }
+
+    if (row < n && grid[row][col] !== '.') {
+        while (row < n && grid[row][col] !== '.') {
+            col += 1 
+
+            if (col === n) {
+                row += 1
+                col = 0
+            }
+        }
+    }
+
+    if (row === n) {
+        return true;
+    }
+
+    for (let val = 1; val < n + 1; val++) {
+
+        let tmp = val.toString();
+        
+        if (validateGrid(grid, tmp, row, col)) {
+            grid[row][col] = tmp;
+
+            if (solveGrid(grid, row, col + 1)) {
+                return true;
+            }                                                             
+        }
+    }
+    grid[row][col] = '.'
 };
 
 const compareGrids = (grid, solution) => {};
 
-export {randomGrid, solveGrid, compareGrids };
+export {randomGrid, solveGrid, compareGrids, validateGrid};
