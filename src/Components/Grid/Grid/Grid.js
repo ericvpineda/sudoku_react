4509ea90745757cdd7019e7c63844241c518b476
Eric Pineda <ericvpineda@berkeley.edu>
2022-10-25 19:42:00 -0700

    implemented -- mousetrap for arrow key, delete functionality

--- src/Components/Grid/Grid/Grid.js
+++ src/Components/Grid/Grid/Grid.js
@@ -7,15 +7,20 @@ import SolvedModal from "../../UI/Modal/SolvedModal/SolvedModal";
 import { useSelector, useDispatch,} from "react-redux";
 import { Children, Fragment, useEffect } from "react";
 import { gridActions } from "../../../store/grid";
+import { cellActions } from "../../../store/cell";
 import { randomGrid } from "../../../utils/utils";
+
+const MouseTrap = require('mousetrap')
 
 const Grid = () => {
   const dispatch = useDispatch();
   const workingGrid = useSelector(state => state.grid.workingGrid);
+  const initialGrid = useSelector(state => state.grid.initialGrid);
   const difficultyModal = useSelector(state => state.grid.difficultyModalActive);
   const isSolved = useSelector(state => state.grid.isSolved);
   const time = useSelector(state => state.grid.time);
   const difficulty = useSelector(state => state.grid.difficulty)
+  const [row, col] = useSelector(state => state.cell.selectedCell)
 
   // Note: Creates initial board
   useEffect(() => {
@@ -28,6 +33,64 @@ const Grid = () => {
   const resetModalHandler = () => {
     dispatch(gridActions.activateDifficultyModal(false));
   };
+
+  const numTrapHandler = (input) => {
+    if (row != null && col != null && initialGrid[row][col] === '.') {
+      dispatch(gridActions.fillCell([input, [row, col]]))
+    }
+  }
+
+  const eraseCellTrapHandler = () => {
+    dispatch(gridActions.eraseCell([row, col]))
+  }
+
+  const moveHandler = (direction) => {
+    
+    if (row != null && col != null) {
+      switch(direction) {
+        case 'up':
+          if (row - 1 >= 0) {
+            dispatch(cellActions.move([row - 1, col]))
+          }
+          break;
+        case 'down':
+          if (row + 1 < workingGrid.length) {
+            dispatch(cellActions.move([row + 1, col]))
+          }
+          break;
+        case 'left':
+          if (col - 1 >= 0) {
+            dispatch(cellActions.move([row, col - 1]))
+          }
+          break;
+        case 'right':
+          if (col + 1 < workingGrid.length) {
+            dispatch(cellActions.move([row, col + 1]))
+          }
+          break;
+        default:
+      }
+    }
+    
+  }
+
+  MouseTrap.bind('1', () => numTrapHandler('1'))
+  MouseTrap.bind('2', () => numTrapHandler('2'))
+  MouseTrap.bind('3', () => numTrapHandler('3'))
+  MouseTrap.bind('4', () => numTrapHandler('4'))
+  MouseTrap.bind('5', () => numTrapHandler('5'))
+  MouseTrap.bind('6', () => numTrapHandler('6'))
+  MouseTrap.bind('7', () => numTrapHandler('7'))
+  MouseTrap.bind('8', () => numTrapHandler('8'))
+  MouseTrap.bind('9', () => numTrapHandler('9'))
+  MouseTrap.bind('backspace', () => eraseCellTrapHandler())
+  MouseTrap.bind('del', () => eraseCellTrapHandler())
+
+  // Note: Arrow key functionality
+  MouseTrap.bind('up', () => moveHandler('up'))
+  MouseTrap.bind('left', () => moveHandler('left'))
+  MouseTrap.bind('down', () => moveHandler('down'))
+  MouseTrap.bind('right', () => moveHandler('right'))
 
   return (
     <Fragment>
